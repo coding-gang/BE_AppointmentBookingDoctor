540e06f54070702b93f5877079a783b8f358c74c
rinrealvngangz <tuannmctk42@gmail.com>
2021-10-18 12:59:49 +0700

    add lightbox img

--- DoctorAppointmentBooking/src/app/components/doctor-file/doctor-file.component.ts
+++ DoctorAppointmentBooking/src/app/components/doctor-file/doctor-file.component.ts
@@ -2,20 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Lightbox } from 'ngx-lightbox';
 
 const PATH ='assets/img/features/';
-
+interface album{
+  src: string,
+  caption: string,
+  thumb:string
+}
 @Component({
   selector: 'app-doctor-file',
   templateUrl: './doctor-file.component.html'
 })
 export class DoctorFileComponent implements OnInit {
-  public _albums:Array<any> = [];
+  public _albums:Array<album> = [];
   constructor(private _lightbox:Lightbox) {
     for (let i = 1; i <= 4; i++) {
       const src = `${PATH}feature-0${i}.jpg`;
-      const caption = 'Image ' + `feature-0${i}.jpg`+ ' caption here';
-      const album = {
+      const caption = 'Image' + `feature-0${i}.jpg`;
+      const album:album = {
          src: src,
-         caption: caption
+         caption: caption,
+         thumb:''
       };
       this._albums.push(album);
     }
