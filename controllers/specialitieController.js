2ab29cf003b60466d8df795fcf1be5e1b063f3f2
Nguyen Trien <trienvaj@gmail.com>
2021-10-25 20:28:24 +0700

    handler error specialities

--- controllers/specialitieController.js
+++ controllers/specialitieController.js
@@ -43,3 +43,21 @@ exports.delete = (req,res) => {
         res.status(204).send();
     })
 }
+
+exports.isExistSpecialities = (req,res,next)=>{
+    const specialityId = req.params.specialityId;
+    const isExistSpecSql = `select isExist_Specialities_func(?) as isExistSpec`;
+    const isExistSpecFromDoctorSql = `select isExist_SpecialitiesFromDoctor_func(?) as isSpecFromDoctor`;
+    const sql = `${isExistSpecSql};${isExistSpecFromDoctorSql};`;
+    connectDb.query(sql,[specialityId,specialityId],(error,result)=>{
+        if(error) throw error;
+         const isExistSpec =  result[0][0].isExistSpec;
+         const isNotExistDoctor = result[1][0].isSpecFromDoctor;
+         if(isNotExistDoctor === 0 && isExistSpec !==0){
+                next();      
+         }else{
+             const err =  new appError(409,"Xoá dữ liệu thất bại");
+             res.status(err.statusCode).send(err.resError().error);
+         }
+    })
+}
--- routes/specialitiesRouters.js
+++ routes/specialitiesRouters.js
@@ -6,7 +6,7 @@ const specialitieController = require('../controllers/specialitieController');
 
 router.route('/specialities').get(specialitieController.getAll);
 router.route('/speciality/:specialityId').put(specialitieController.update);
-router.route('/speciality/:specialityId').delete(specialitieController.delete);
+router.route('/speciality/:specialityId').delete(specialitieController.isExistSpecialities , specialitieController.delete);
 router.route('/speciality/:specialityId').get(specialitieController.getById);
 
 module.exports =router;
