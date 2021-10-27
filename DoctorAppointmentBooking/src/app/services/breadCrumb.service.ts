2f86b75753b055abaada54b5e4d9bbeb19f50f9c
rinrealvngangz <tuannmctk42@gmail.com>
2021-10-27 10:32:06 +0700

    add breadCrumb fix bug success

--- DoctorAppointmentBooking/src/app/services/breadCrumb.service.ts
+++ DoctorAppointmentBooking/src/app/services/breadCrumb.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { of ,from } from "rxjs";
+import { map , findIndex ,take } from "rxjs/operators";
 import { IBreadCrumb } from "../components/bread-crumb-bar/bread-crumb.model";
 
 let BreadCrumbs:IBreadCrumb[] =[];
@@ -14,10 +16,19 @@ export class BreadCrumbsService{
       let lable = isExistRouteConfig ?
                   activatedRoute.routeConfig?.data?.breadcrumb :'';
       let url = isExistRouteConfig ? router.url :'';
-      console.log(lable);
-      console.log(url);
-     // BreadCrumbs.push({lable,url});
-     BreadCrumbs = [...BreadCrumbs,{lable,url}]
-      return BreadCrumbs;
+      let indexExist =0;
+      const isExist =  from(BreadCrumbs)
+          .pipe(
+            findIndex((el)=> el.url === url)
+            );
+            isExist.subscribe(index => indexExist =index);
+            if(indexExist !== -1){
+                of(BreadCrumbs)
+                .pipe(map(el => el.slice(0,indexExist+1)))
+                .subscribe(el => BreadCrumbs = [...el]);
+            }else{
+              BreadCrumbs = [...BreadCrumbs,{lable,url}]
+            }
+          return BreadCrumbs;
   }
 }
