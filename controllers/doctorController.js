ba6bbc960cea51d0942860563b4b9e68b26bedf9
Nguyen Trien <trienvaj@gmail.com>
2021-11-08 10:33:28 +0700

    new commit

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,6 +1,9 @@
 const { add } = require('nodemon/lib/rules');
 const connectDb = require('../utils/connectionDB');
 const appError = require('../utils/appError');
+const encryptPass = require('../utils/encrypt');
+const decryptPass = require('../utils/decrypt');
+const decrypt = require('../utils/decrypt');
 exports.getAll = (req,res,next) =>{
     let sql = "select * from ViewDoctor";
     connectDb.query(sql,(error, results, fields) =>{
@@ -22,17 +25,23 @@ exports.getById = (req,res)=>{
     })
 }
 
-exports.Add = (req,res)=>{
+exports.Add = async (req,res)=>{
+       
        const sql = "call Add_Doctor_Proc(?,?,?,?,?,?,?,?,?,?)";
        const doctor =  req.body;
        doctor.DOB = doctor.DOB.split("/").reverse().join("-");
        doctor.gender = doctor.gender ===1 ? true : false;
        const params = [];
+        const encrypt = new encryptPass(doctor.password);
+        const hash = await encrypt.encryptFunc();
+        doctor.password = hash;
        Object.values(doctor).forEach(el => params.push(el));
+     
+
        connectDb.query(sql,params,(error,results)=>{
         if (error) throw error;
        const doctor =results[0][0];
-        res.status(201).json({status:'success',doctor});
+        res.status(201).json({status:'success',message:"Thêm dữ liệu thành công",doctor});
        })    
 }
 
@@ -63,20 +72,39 @@ exports.updatePass = (req,res) => {
     const id = req.params.doctorId;
     const newpass = req.body.newPass;
     console.log(newpass);
+    const encrypt = new encryptPass(newpass);
+    const newPassEncrypth= encrypt.encryptFunc();
     const sql = "call Update_Password_Doctor_Proc(?,?)";
-    connectDb.query(sql,[newpass,id],(error)=>{
+    connectDb.query(sql,[newPassEncrypth,id],(error)=>{
         if(error) throw error;
-        res.status(200).send({message:"Cập nhật mật khẩu thành công"});
+        res.status(200).json({status:"success",message:"Cập nhật mật khẩu thành công"});
     })
 }
 exports.checkExistPass = (req,res,next) => {
      const id = req.params.doctorId;
-    const newpas = req.body.password;
+    const newPass = req.body.newPass;
+    const oldPass = req.body.oldPass;
+    let oldPassDb = "";
+   
+
+            
+              decryptFromDB(id).then(val => {
+                  console.log(val);
+              })
+           
+        
     const sql = "select isExistPassOfDoctor_Func(?,?) as exist";
-    connectDb.query(sql,[id,newpas],(error,result) =>{
+    connectDb.query(sql,[id,'abc'],(error,result) =>{
         if(error) throw error;
        ({exist,...rest}=result[0]);
-        if(exist ===1) next();
+        if(exist === 1){
+            if(oldPass === newPass) {
+                const appErr = new appError(409,"Mật khẩu mới trùng mật cũ");
+            res.status(appErr.statusCode).send(appErr.resError().error);
+            } else{
+                next();
+            }
+        }
         else {
             const appErr = new appError(409,"Nhập sai mật khẩu");
             res.status(appErr.statusCode).send(appErr.resError().error);
@@ -97,3 +125,16 @@ exports.checkExistUserName = (req,res,next) =>{
         }
     })
 }
+
+ const decryptFromDB = (id) =>{
+    const sql = "select getPassWord_Func(?) as result";
+    
+   connectDb.query(sql,id,(err,rs)=>{
+        if(err) throw err;
+        return new Promise(function(resolve,reject){
+              
+               resolve(rs[0].result);
+        // return cbGetPassword(rs[0].result);
+        })
+    }
+   )}
