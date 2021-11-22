a6b70ba7c8daeb5cbcdbe49160759cfc191529b1
Nguyen Trien <trienvaj@gmail.com>
2021-11-22 08:34:28 +0700

    crud-appointment

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -2,6 +2,15 @@ const connectDb = require('../utils/connectionDB');
 const appError = require('../utils/appError');
 
 
+exports.getById = (req,res) =>{
+    const appointmentId = req.params.appointmentId;
+    const sql = "select * from appointmentDoctorView where appointmentId=(?)";
+    connectDb.query(sql,appointmentId,(err, rs)=>{
+        if(err) throw err;
+        res.status(200).json({status:"success",appointment:rs})
+    })
+}
+
 exports.getAllApointmentPatients = (req,res) =>{
     const sql = "select * from appointmentPatientView"
     connectDb.query(sql,(err,rs)=>{
@@ -16,4 +25,31 @@ exports.getAllApointmentDoctors = (req,res) =>{
         if (err) throw err;
         res.status(200).json({status:"success",appointments:rs})
     })
+}
+exports.insert = (req,res) =>{
+    const sql = "call Add_Appointments_Proc(?,?)";
+     ({scheduleId, patientId}= req.body);
+    connectDb.query(sql,[scheduleId, patientId],(err,rs)=>{
+        if(err) throw err;
+        res.status(200).json({status:"success",message:"Đặt lịch khám thành công!"})
+    })
+}
+
+exports.update=(req,res) =>{
+    const sql = "call Update_Appointments_Proc(?,?,?)";
+    const appointmentId = req.params.appointmentId;
+     ({scheduleId, patientId}= req.body);
+     console.log(appointmentId);
+    connectDb.query(sql,[appointmentId,scheduleId, patientId],(err,rs)=>{
+        if(err) throw err;
+        res.status(200).json({status:"success",message:"Cập nhật lịch khám thành công!"});
+        })
+}
+exports.delete = (req,res) =>{
+    const sql = "call Del_Appointments_Proc(?)";
+    const appointmentId = req.params.appointmentId;
+    connectDb.query(sql,appointmentId,(err,rs)=>{
+        if(err) throw err;
+        res.status(200).json({status:'success',message:'xoá lịch khám thành công!'});
+        })
 }
--- routes/appoinmentRouter.js
+++ routes/appoinmentRouter.js
@@ -6,6 +6,10 @@ const router = express.Router();
 const appointController = require('../controllers/appointmentController');
 const passport =require('passport');
 router.route('/appointments-patient').get(appointController.getAllApointmentPatients);
-router.route('/appointments-doctor').get(appointController.getAllApointmentDoctors);
+router.route('/appointments-doctor').get(appointController.getAllApointmentDoctors)
+router.route('/appointment').post(appointController.insert);
+router.route('/appointment/:appointmentId').put(appointController.update)
+.delete(appointController.delete)
+.get(appointController.getById);
 
 module.exports = router;
